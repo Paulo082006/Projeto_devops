4c4b5e2944a798830d92ec843d9be26dd5c6a252
dougao123 <zeningem20@gmail.com>
2025-11-23 19:29:12 -0300

    implementação prometheus

---
+++ dockerfile
@@ -0,0 +1,18 @@
+
+FROM python:3.10-slim
+
+# 2. Defina o diretório de trabalho dentro do contêiner
+WORKDIR /app
+
+# 3. Copie o arquivo de requisitos
+COPY requirements.txt .
+
+# 4. Instale TODAS as bibliotecas (incluindo psycopg2)
+# O --no-cache-dir mantém a imagem um pouco menor
+RUN pip install --no-cache-dir -r requirements.txt
+
+# 5. Copie o resto do seu código (ex: app.py)
+COPY . .
+
+CMD ["flask", "run", "--host=0.0.0.0"]
+# O comando para rodar a aplicação será dado pelo docker-compose.yml
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from dotenv import load_dotenv
 from flask_cors import CORS
+from prometheus_flask_exporter import PrometheusMetrics  # <-- IMPORTANTE
 from db import db
 import os
 
@@ -10,12 +11,23 @@ load_dotenv()
 
 # Configuração da aplicação Flask
 app = Flask(__name__)
+
+# Habilitar CORS
 CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "http://127.0.0.1:5000"}})
+
+# Banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 db.init_app(app)
+
+# Prometheus - cria automaticamente o endpoint /metrics
+metrics = PrometheusMetrics(app) 
+
+with app.app_context():
+    db.create_all()
 
 # Registro das Blueprints
 app.register_blueprint(clientes_bp)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
